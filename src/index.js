261c2822d562deee9cc384294fd4fdb41dbedfe3
beelee3 <146008646+beelee3@users.noreply.github.com>
2023-11-16 13:07:40 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { Dictionary } from "./logic/Dictionary.js";
+
+
+var dicHira = new Dictionary("hiragana");
+
+dicHira.initialize();
+dicHira.display();
+
+// var characterImg = document.getElementById("character");
+var dicH = dicHira.getDictionary();
+// document.getElementById("character").src = dicH[dicHira.randomSelectKey()].imgSrc;
+
+document.getElementById("characterImg").src = "../src/pictures/hiragana/chi-chi-h.png";
+
+
+/*console.log(`//////////`);
+let dicH = dicHira.getDictionary();
+let rKeyH = dicHira.randomSelectKey();
+console.log(`new Rng key select:${dicH[rKeyH].romaji}`);
+*/
+
+// console.log(dicHira.getKeyArr());
+// console.log(dicKata.getKeyArr());
+
+// dicHira.randomSelectKey();
+// dicKata.randomSelectKey();
+
+
