8db062a3464da268a4997d3a84e355eec109a77c
beelee3 <146008646+beelee3@users.noreply.github.com>
2023-12-14 16:21:38 -0500

    Revert "isolate character on click in chart.js"
    This reverts commit 8f3323849eca72eede4a6b1119aaf84ccd72e91e.

--- electron/chart/chart.js
+++ electron/chart/chart.js
@@ -1,8 +1,10 @@
-import {init,showAll,noteHandler,dislayChar} from "./chartLogic.js";
+// import {init,showAll,showToggled,ToggleState,currentToggleState} from "./chartLogic.js";
 
 // var currentToggleState;
 
-
+var dicKata;
+var dicHira;
+const imgAddon = `../.`
 
 const ToggleState = {
     all:'all',
@@ -12,23 +14,102 @@ const ToggleState = {
     e: 'e',
     o: 'o',
 };
+
 var currentToggleState;
+var importedArrayHira = [], importedArrayKata = [];
 
 
 
-export function showToggled(){
+async function init(){
+    console.log(` chart.js`); 
+    dicKata =  await window.electronApi.getDictKata();
+    dicHira =  await window.electronApi.getDictHira();
+
+    console.log(dicHira);
+    console.log(dicKata);
+    display();
+    noteHandler();
+ }
+
+function display(){
+    var targetDiv = document.getElementById("dHira");
+
+    for(var key in dicHira){
+        // const divElement  = document.createElement("div");
+        const divElement  = document.createElement("button");
+        divElement.className = "centerV centerH character";
+        divElement.id = `${key}`;
+
+        const paraElement  = document.createElement("p");
+
+        //content that goes into p tag
+        const nodeImg = document.createElement("img")
+            nodeImg.src = imgAddon+dicHira[key].imgSrc;
+            nodeImg.id = "imgChart";
+
+        // const node  = document.createTextNode(`Romaji: [${dicHira[key].romaji}]  \n Pronunciation: [${dicHira[key].pronunciation}] \n Notes: [${dicHira[key].notes}]`);
+        // const node1  = document.createTextNode(`Romaji: [${dicHira[key].romaji}]`);
+        // const node2  = document.createTextNode(`Pronunciation: [${dicHira[key].pronunciation}]`);
+        // const node3  = document.createTextNode(`Notes: [${dicHira[key].notes}]`);
+
+        const node1  = document.createElement("p");
+        node1.textContent = `Romaji: [${dicHira[key].romaji}]`;
+        node1.className = "romaji"
+
+        const node2  = document.createElement("p") 
+        node2.textContent = `Pronunciation: [${dicHira[key].pronunciation}]`;
+
+        const node3  = document.createElement("p");
+        node3.textContent = `Notes: [${dicHira[key].notes}]`;
+
+        //append textNode contents into p
+        // paraElement.appendChild(node);
+        paraElement.appendChild(node1);
+        // paraElement.appendChild(document.createElement("br"));
+        paraElement.appendChild(node2);
+        // paraElement.appendChild(document.createElement("br"));
+        paraElement.appendChild(node3);
+
+        //append img/p into div
+        divElement.appendChild(nodeImg)
+        divElement.appendChild(paraElement);
+         //append div into targeted div
+        targetDiv.appendChild(divElement);
+
+        //add elements to an array;
+
+        importedArrayHira.push(dicHira[key]);
+    }
+}
+
+//render functions
+//notes function 
+    //show only the notes of a clicked character
+    //hide everything else
+
+
+//Toggleing stuff
+function showAll(){
+    //show all elements in the chart
+    const targetDiv = document.getElementById("dHira");
+    const targetDivChildren = targetDiv.querySelectorAll('.character');
+
+    for(let i=0; i<targetDivChildren.length; i++){
+        targetDivChildren[i].style.display = "flex";
+    }
+}
+function showToggled(){
     //show only the items on the Toggleed chart
     console.log(typeof currentToggleState);
-    // if(currentToggleState == currentToggleState.all){
-    //     return false;
-    // }
+    if(currentToggleState == currentToggleState.all){
+        return false;
+    }
         
     
     const targetDiv = document.getElementById("dHira");
     const targetDivChildren = targetDiv.querySelectorAll('.character');
     console.log(targetDivChildren);
 
-    dislayChar.style.display = "none";
     for(let x=0; x<targetDivChildren.length; x++){
         if(!String(targetDivChildren[x].id).includes(currentToggleState)){
             document.getElementById(String(targetDivChildren[x].id)).style.display = "none";
@@ -39,6 +120,26 @@ export function showToggled(){
             
     }
 }
+
+//if you click on a character div, it will expand the div and show the notes
+function noteHandler(){
+    const targetDiv = document.getElementById("dHira");
+    const targetDivChildren = targetDiv.querySelectorAll('.character');
+    console.log(targetDivChildren);
+
+    for(let i = 0; i < targetDivChildren.length; i++){
+        targetDivChildren[i].addEventListener('click',function(){
+            //this is important, without this the function is trying to call data that hasnt been created yet
+            if(this === undefined){
+                return false;
+            }
+            this.style.display = "none";
+            console.log(`was clicked: ${this.id}`);
+        });
+    }
+}
+
+
 
 
 //// buttons
